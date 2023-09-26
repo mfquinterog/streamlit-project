79e2440eb8574e14ac519775a73c91fc0f471be6
datascienceanywhere <datascienceanywhere@gmail.com>
2023-09-27 00:33:28 +0530

    modify

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 import pandas as pd 
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import sys
 import os
